24c3d3da69f8f2e95e8c25eac90ec6becbac30d9
Jan Burger <jan.burger98@web.de>
2023-04-18 20:43:58 +0200

    testing

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import requests
+import datetime as dt
 
 
-url = "https://apewisdom.io/api/v1.0/filter/all-stocks/page/4"
+url = "https://apewisdom.io/api/v1.0/filter/all-stocks/page/56"
 
 response = requests.get(url)
 
-print(response.json)
+#print(response.json())
+
+df = pd.DataFrame(response.json()["results"])
+
+print(df.head())
