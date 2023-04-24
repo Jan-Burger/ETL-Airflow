ae300ee60fffd7feaa76930e333a26450caa3b46
Jan Burger <jan-burger@t-online.de>
2023-04-24 10:59:40 +0200

    database setup refactored

--- Database_setup/create_staging_area_tables.sql
+++ Database_setup/create_staging_area_tables.sql
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS public.stocks_sentiment_agg_by_day
 CREATE TABLE IF NOT EXISTS public.stocks_sentiment_details
 (
     id serial NOT NULL,
+    ticker_symbol character varying(10) NOT NULL,
     date_pulled timestamp without time zone NOT NULL,
     date_time_mentions timestamp without time zone NOT NULL,
     mentions integer NOT NULL,
